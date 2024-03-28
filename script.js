8f207f651326c5a2e4eec152b02d5b2bab2ae86a
QuickMythril <quickmythril@protonmail.com>
2024-03-28 17:34:57 -0400

    v0.2.0 - Edit images, audio, & video

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let userPublicKey = '';
 let userAddress = '';
 let userName = '';
+const infoDetails =
+    `<img src="red-x.svg" style="width:15px;height:15px;">
+    Click the identifier to "delete" content.<br>
+    (This will replace it with a blank file.)<br>
+    <br>
+    <img src="file-up.png" style="width:15px;height:15px;">
+    Click the file select icon to "edit" content.<br>
+    (This will replace it with a selected file.)`
 
 document.getElementById('login-button').addEventListener('click', accountLogin);
 
@@ -21,7 +29,7 @@ async function accountLogin() {
             });
         }
         userName = names.length > 0 && names[0].name ? names[0].name : 'Name unavailable';
-        document.getElementById('info-details').innerHTML = 'Select an item to "delete" it.';
+        document.getElementById('info-details').innerHTML = infoDetails;
         document.getElementById('account-details').innerHTML = `${userAddress}<br>${userName}`;
         fetchContent();
     } catch (error) {
@@ -71,27 +79,39 @@ async function fetchContent() {
                 }
                 let sizeString = formatSize(result.size);
                 tableHtml += `<tr><td>${result.service}</td>
-                    <td class="clickable-name" data-service="${result.service}" data-identifier="${identifier}">
-                    ${identifier}</td><td>`;
+                    <td><span class="clickable-delete" data-service="${result.service}" data-identifier="${identifier}">
+                    <img src="red-x.svg" style="width:15px;height:15px;">${identifier}</span></td><td`;
                 if ((result.service === 'THUMBNAIL') ||
                 (result.service === 'QCHAT_IMAGE') ||
                 (result.service === 'IMAGE')) {
-                    tableHtml += `<img src="/arbitrary/${result.service}/${userName}/${identifier}"
+                    tableHtml += `><img src="file-up.png" style="width:40px;height:40px;"
+                    class="clickable-edit" data-service="${result.service}" data-identifier="${identifier}">
+                    <img src="/arbitrary/${result.service}/${userName}/${identifier}"
                     style="width:100px;height:100px;"
                     onerror="this.style='display:none'"
                     ></img>`
                 } else if (result.service === 'VIDEO') {
-                    tableHtml += `<video width="100%" controls>
+                    tableHtml += `><img src="file-up.png" style="width:40px;height:40px;"
+                    class="clickable-edit" data-service="${result.service}" data-identifier="${identifier}">
+                    <video controls>
                     <source src="/arbitrary/${result.service}/${userName}/${identifier}">
                     </source></video>`
                 } else if ((result.service === 'AUDIO') ||
                 (result.service === 'QCHAT_AUDIO') ||
                 (result.service === 'VOICE')) {
-                    tableHtml += `<audio controls>
+                    tableHtml += `><img src="file-up.png" style="width:40px;height:40px;"
+                    class="clickable-edit" data-service="${result.service}" data-identifier="${identifier}">
+                    <audio controls>
                     <source src="/arbitrary/${result.service}/${userName}/${identifier}">
                     </source></audio>`
+                } else if ((result.service === 'BLOG') ||
+                (result.service === 'BLOG_POST') ||
+                (result.service === 'BLOG_COMMENT')) {
+                    tableHtml += `><embed width="100%" type="text/html"
+                    src="/arbitrary/${result.service}/${userName}/${identifier}">
+                    </embed>`
                 } else {
-                    tableHtml += `<embed width="100%" type="text/html"
+                    tableHtml += `><embed width="100%" type="text/html"
                     src="/arbitrary/${result.service}/${userName}/${identifier}">
                     </embed>`
                 }
@@ -105,11 +125,18 @@ async function fetchContent() {
             document.getElementById('content-details').innerHTML = tableHtml;
             document.getElementById('account-details').innerHTML += `<p>Total Files: ${totalFiles}</p>
             <p>Total Size: ${totalSizeString}</p>`;
-            document.querySelectorAll('.clickable-name').forEach(element => {
+            document.querySelectorAll('.clickable-delete').forEach(element => {
                 element.addEventListener('click', function() {
                     let targetService = this.getAttribute('data-service');
                     let targetIdentifier = this.getAttribute('data-identifier');
                     deleteContent(targetService, targetIdentifier);
+                });
+            });
+            document.querySelectorAll('.clickable-edit').forEach(element => {
+                element.addEventListener('click', function() {
+                    let targetService = this.getAttribute('data-service');
+                    let targetIdentifier = this.getAttribute('data-identifier');
+                    editContent(targetService, targetIdentifier);
                 });
             });
         } else {
@@ -153,5 +180,36 @@ async function deleteContent(service, identifier) {
         console.log('Content deleted successfully');
     } catch (error) {
         console.error('Error deleting content:', error);
+    }
+}
+
+async function editContent(service, identifier) {
+    try {
+        if (!userName || userName === 'Name unavailable') {
+            return;
+        }
+
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.click();
+        const selectedFilePromise = new Promise((resolve, reject) => {
+            input.onchange = (event) => {
+                const file = event.target.files[0];
+                resolve(file);
+            };
+            input.onerror = reject;
+        });
+        const selectedFile = await selectedFilePromise;
+        const editIdent = (identifier === 'default') ? '' : identifier;
+        const response = await qortalRequest({
+            action: "PUBLISH_QDN_RESOURCE",
+            name: userName,
+            service: service,
+            identifier: editIdent,
+            file: selectedFile
+        });
+        console.log('Content edited successfully');
+    } catch (error) {
+        console.error('Error edited content:', error);
     }
 }
